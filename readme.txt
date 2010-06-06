fa3b3205728b10e395afc8391ddedb6c6c44e20c
derflash <derflash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-06-06 19:08:53 +0000

    initial release
    git-svn-id: https://plugins.svn.wordpress.org/tagcloud-html5/trunk@249562 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,30 @@
+=== TagCloud html5 ===
+Contributors: derflash
+Donate link: https://flattr.com/thing/17626/bytefrog
+Tags: tag, cloud
+Requires at least: 2.9
+Tested up to: 2.9
+Stable tag: trunk
+
+Tag cloud on html5 steroids based on tagcanvas-JS from www.goat1000.com
+
+== Installation ==
+
+
+1. Download the zip file
+2. Unzip it to your wordpress plugin directory
+3. Activate it
+4. Put the widget somewhere into your sidebar
+
+== Screenshots ==
+
+1. tagCloudHtml5.png
+
+== Changelog ==
+
+= 1.0 =
+* initial release
+
+== Known issues ==
+
+The iPad has some problems with the canvas element, so actually the plugin detects those and just shows the standard tag cloud then…
---
+++ tagCloud-html5.php
@@ -0,0 +1,83 @@
+<?php
+/*
+Plugin Name: TagCloud html5
+Plugin URI: http://www.derflash.de
+Description: Tag cloud on html5 steroids based on tagcanvas-JS from www.goat1000.com
+Version: 1.0
+Author: Bjoern Teichmann
+Author URI: http://www.derflash.de
+Update Server: http://www.derflash.de
+Min WP Version: 2.9
+License: LGPL v3
+*/
+
+
+add_action('wp_head', 'tagCloudHeadCB');
+function tagCloudHeadCB() {
+	if (!strpos($_SERVER['HTTP_USER_AGENT'],'iPad')) { ?>
+    
+	<script src="<?php echo get_option('siteurl'); ?>/wp-content/plugins/tagcloud-html5/tagcanvas.js" type="text/javascript"></script>
+    
+    <script type="text/javascript">
+	<!--
+	
+	function addLoadEvent(your_function) {
+		if (window.attachEvent) {window.attachEvent('onload', your_function);}
+		else if (window.addEventListener) {window.addEventListener('load', your_function, false);}
+		else {document.addEventListener('load', your_function, false);}
+	}
+	
+	function tweetfeed_load() {
+		TagCanvas.maxSpeed = 0.035;
+		TagCanvas.textColour = '#2d83d5';
+		TagCanvas.outlineColour = '#2d83d5';
+	    TagCanvas.Start('tagCloudh5');
+	}
+	
+	addLoadEvent(tweetfeed_load);
+	
+	-->
+    </script>
+    <?php
+    }
+}
+
+
+
+
+/*********** WIDGETS ***********/
+
+add_action('plugins_loaded', 'tagCloudLoad');
+function tagCloudLoad() {
+	if (!function_exists('register_sidebar_widget')) return;	
+	register_sidebar_widget('Tag Cloud html5', 'widget_tagCloud');	
+}
+function widget_tagCloud() {
+
+	echo $before_widget;
+	?>
+	<li><div id="tch5" class="widget widget_tagCloud_h5">
+	<?php echo $before_title; ?>
+	
+	<h2 class="title">Tag Cloud</h2>
+	<?php
+	
+	// show standard tag cloud on ipad until the h5 cloud is fixed
+	if (strpos($_SERVER['HTTP_USER_AGENT'],'iPad')) { ?>
+		<div width="260" height="260">
+			<?php wp_tag_cloud(); ?>
+		</div>
+	<?php } else { ?>
+		<canvas width="260" height="260" id="tagCloudh5">
+			<?php wp_tag_cloud(); ?>
+		</canvas>
+	<?php }
+	
+	?>
+	</div>
+	</li>
+	<?php
+	echo $after_widget;
+}
+
+?>
